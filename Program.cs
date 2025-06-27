99ee21c341e4b82f283481cdc7771a0822195ef6
gofffro <vazheninstepan@gmail.com>
2025-06-27 11:11:28 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace singleton
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Singleton singleton = Singleton.GetInstance();
+      singleton.SayHello();
+    }
+  }
+
+  public class Singleton
+  {
+    private static readonly Singleton instance = new Singleton();
+
+    private Singleton() { }
+
+    public static Singleton GetInstance()
+    {
+      return instance;
+    }
+
+    public void SayHello()
+    {
+      Console.WriteLine("Привет из Singleton!");
+    }
+  }
+}
